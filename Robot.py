112b02cf87ffdd95595f436260c9189edfb2cb93
Liorén <lior@goliathan>
2019-07-23 08:35:29 -0300

    Definiendo atributos y métodos de robot, desiciones de diseño por tomargit add Robot.py!

--- Robot.py
+++ Robot.py
@@ -1,4 +1,6 @@
-# coding=UTF-8
+# coding=UTF-
+import numpy as np
+
 from Laberinto import *
 
 class Robot(object):
@@ -14,19 +16,50 @@ class Robot(object):
 
    
 
-  velocidad  (private)
+  position  (private)
 
   """
-  def __init__(self, velocidad):
-      self.velocidad=velocidad
+      #add atributes: relative_posit
+      """ El parámetro map_size debe ser suministrdo en el main, habiendo sido adquirido previamente
+       a través de un metodo (get_size) del laberinto"""
+       
+  def __init__(self, map_size, speed, curr_position, sensors):
+      #position: inicio (origen de coordenadas para Ava) y recorrido del objeto
+      self.curr_position=(0, 0)
+      """inicializar el array de desiciones de posiciones recorridas guardando 
+        en una lista.
+         Request maze for variables  [forward, right , backwards, left]=[i,j, k,  l] with i,j,k,l=1, 0
+        check maze map relative to ADA!!!!!_"""
+      self.sensors=[0, 0 ,0 ,0]
+
+      #4-FACED ADA: has 4 faces and no orientation
+      #self.orientation
+
+      # TODO: Consultar dimensiones del mapa de maze
+      #starting array filled with zeros
+      #map size need getter in : map_size.
+      self.memory_map = np.zero(map_size, map_size)
+  
+  
+  def sensar(self, sensors):
       
-  def decidir(self):
-    """
-     
+  """
+    
+
+  @return  :
+  @author
+  """
+  
+  pass    
+      
+  def decidir(self): 
+    """ 
+
 
     @return  :
     @author
     """
+    
     pass
 
   def desplazar(self):
@@ -35,17 +68,13 @@ class Robot(object):
 
     @return  :
     @author
+    
     """
+    #self.speed = 1
+    #from (i,j) to (i+speed, j)
     pass
 
-  def sensar(self):
-    """
-     
 
-    @return  :
-    @author
-    """
-    pass
 
 
 
