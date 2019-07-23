d52fc293c00ce6441fcbf1c45e28cbef9c2c14e7
Liorén <lior@goliathan>
2019-07-23 18:07:07 -0300

    Esqueleto de nuevos atributos y métodos de Robot. Implementación del sensado y movimientos utilizando listas, setters sin definir

--- Robot.py
+++ Robot.py
@@ -23,56 +23,106 @@ class Robot(object):
       """ El parámetro map_size debe ser suministrdo en el main, habiendo sido adquirido previamente
        a través de un metodo (get_size) del laberinto"""
        
-  def __init__(self, map_size, speed, curr_position, sensors):
-      #position: inicio (origen de coordenadas para Ava) y recorrido del objeto
-      self.curr_position=(0, 0)
-      """inicializar el array de desiciones de posiciones recorridas guardando 
-        en una lista.
-         Request maze for variables  [forward, right , backwards, left]=[i,j, k,  l] with i,j,k,l=1, 0
-        check maze map relative to ADA!!!!!_"""
-      self.sensors=[0, 0 ,0 ,0]
-
-      #4-FACED ADA: has 4 faces and no orientation
-      #self.orientation
-
-      # TODO: Consultar dimensiones del mapa de maze
-      #starting array filled with zeros
-      #map size need getter in : map_size.
-      self.memory_map = np.zero(map_size, map_size)
-  
-  
-  def sensar(self, sensors):
+  def __init__(self):
+      #position: inicio (origen de coordenadas para Ava)
       
-  """
+      """
+      """
+      self.sensor=[0, 0 ,0 ,0]
+      self.direccion= None
+      self.velocidad=[0, 0 , 0, 0]
+
+  #sensar is setter
+  def sensar(self, sensor):
     
+   """
+    la variable sensor es tipo lista de 4 elementos: [ , , , ] y
+    toma su valor del método get_neighbors de Laberinto
 
   @return  :
   @author
   """
   
-  pass    
-      
-  def decidir(self): 
-    """ 
-
-
-    @return  :
-    @author
+  self.sensor= sensor
+  
+  def set_direccion(self, direccion):
+   """
+   direccion =  [ arriba, derecha, abajo , izquierda]  indica hacia donde se va a mover el robot
+   comienza con None y el main produce el cambio 
+   @return  :
+   @author
+   """ 
+   self.direccion = direccion   
+  
+       
+  def seguir_pared(self):
     """
-    
-    pass
-
-  def desplazar(self):
-    """
+    Toma de desiciones para salir del laberinto: algoritmo wall follow
      
 
     @return  :
     @author
     
     """
+    j = np.nonzero(self.direccion)[0][0]
+    sensor_front = self.sensor[j]
+    sensor_left = self.sensor[j-1]
+    sensor_right = self.sensor[j-3]
+
+    #caso que sigue de largo, ya tengo una dirección
+    if (sensor_front == 0 and sensor_left != 0):
+        #self.dirección = self.direccion
+
+    elif(sensor_front !=0 and sensor_left == 0):
+      #permutación cíclica en dirección , mover el 1 al indice anterior al que está
+      #moverse hacia la izquierda
+       self.direccion = turn_left(self.direccion)
+   
+    elif(sensor_front !=0 and sensor_left != 0 and sensor_right == 0):
+      #moverse hacia la derecha
+      #permutación cíclica en dirección , mover el 1 al indice j-3
+      self.direccion = turn_right(self.direccion)
+      
+    elif(sensor_front != 0 and sensor_left !=0 and sensor_right != 0)
+      """moverse hacia atrás
+      permutación cíclica en dirección, mover el 1 al índice j-2"""
+      
+      self.direccion = turn_back(self.direccion)
+        
+
+  def set_speed(self):
     #self.speed = 1
     #from (i,j) to (i+speed, j)
-    pass
+    pass 
+  
+#---------------------------------------------------------------------------------------------------
+#------------------------------------FUNCIONES------------------------------------------------------
+
+
+def turn_right(direccion):
+    """
+    turn_right(direccion):
+        Toma la lista direccion y hace una permutacion ciclica de manera
+        que la nueva direccion sea hacia la derecha de la direccion anterior
+
+        INPUT:
+        direccion: lista de 4 elementos (CEROS O UNOS)
+
+        OUTPUT:
+        lista de 4 elementos
+
+        lo mismo para turn_back y turn_left
+    """
+    return [direccion[i-1] for i in range(len(direccion))]
+
+def turn_back(direccion):
+    return [direccion[i-2] for i in range(len(direccion))]
+
+def turn_left(direccion):
+    return [direccion[i-3] for i in range(len(direccion))]
+
+    
+    
 
 
 
