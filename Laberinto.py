1d6c9162fc1720ec625cc9788ca736ea57375d38
hchanampe <hugogher@gmail.com>
2019-07-23 21:27:52 -0300

    desarrollando el metodo generar_obstaculos, obteniendo los lugares donde puedo insertar obtaculos en la matriz

--- Laberinto.py
+++ Laberinto.py
@@ -4,79 +4,88 @@ from Obstaculo import *
 import numpy as np
 class Laberinto(object):
 
-  """
-  :version: Camino de la Solución. (requisito para que el laberinto tenga solución)
-  :author: Lucas Farigliano y Hugo Chanampe
-  """
-
-  """ ATTRIBUTES
-
-   
-
-  size_x  (private)
-
-   
-
-  size_y  (private)
-
-   
-
-  ada_boot  (private)
-
-   
-
-  lista_obstaculos_  (private)
-
-  """ 
-  def __init__(self,dim_x,dim_y):
-     self.dim_x=dim_x 
-     self.dim_y=dim_y 
-     self.mapa= np.ones((self.dim_x+1,self.dim_y+1))
-
-  def generar_solucion(self):
     """
-    @return  : el camino solución del laberinto
-    @author  Lucas Farigliano
-
-    La matriz parte de todos ceros. Lo primero que hace el método es 
-    cambiar los 0 por 3 para que se pueda trabajar mas facilmente.
+    :version: Camino de la Solución. (requisito para que el laberinto tenga solución)
+    :author: Lucas Farigliano y Hugo Chanampe
     """
+
+    """ ATTRIBUTES
     
-    pass
-
-  def generar_obstaculos(self):
+       
+    
+    size_x  (private)
+    
+       
+    
+    size_y  (private)
+    
+       
+    
+    ada_boot  (private)
+    
+       
+    
+    lista_obstaculos_  (private)
+    
     """
+
+    def __init__(self, dim_x, dim_y):
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.mapa = np.full((self.dim_x+2, self.dim_y+2), 2)
+
+    def generar_solucion(self):
+        """
+        @return  : el camino solución del laberinto
+        @author  Lucas Farigliano
+
+        La matriz parte de todos ceros. Lo primero que hace el método es
+        cambiar los 0 por 3 para que se pueda trabajar mas facilmente.
+        """
+    
+        pass
+
+    def generar_obstaculos(self):
+        """
+        @return  :
+        @author Hugo Chanampe
+        codificacion del mapa
+
+        2 - pared
+        1 - obstaculos
+        0 - solucion
+        """
+        x = self.dim_x - 2
+        y = self.dim_y - 2
+
+        cantidad_elementos_solucion = np.count_nonzero(self.mapa == 0)
+        cantidad_obstaculos = (self.dim_x*self.dim_y) - cantidad_elementos_solucion
+
+
+
+    def generar_ada(self, ada_robot):
+        """
      
 
-    @return  :
-    @author
-    """
-    pass
+        @param Robot ada_robot :
+        @return  :
+        @author
+        """
+        pass
 
-  def generar_ada(self, ada_robot):
-    """
+    def actualizar_laberinto(self, x, y):
+        """
      
 
-    @param Robot ada_robot : 
-    @return  :
-    @author
-    """
-    pass
+        @param int x :
+        @param int y :
+        @return  :
+        @author
+        """
+        pass
 
-  def actualizar_laberinto(self, x, y):
-    """
-     
-
-    @param int x : 
-    @param int y : 
-    @return  :
-    @author
-    """
-    pass
-
- '''
+'''
  laberinto debe tener get_size (tamaño del laberinto)
                       get_neighbor_map (devuelve lista de 4 0 o 1)
-                      
- '''
+'''
 
