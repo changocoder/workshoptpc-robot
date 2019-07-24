61ac1ec4f28326381397bbb6f13e08b5eb05753c
Santiago Maldonado Ochoa <santimaldonadoochoa@gmail.com>
2019-07-24 15:12:16 -0300

    Empiezo a trabajar en el archivo Visualizacion.py

--- Visualizacion.py
+++ Visualizacion.py
@@ -29,3 +29,54 @@ class Visualizacion(object):
     @author
     """
     pass
+
+#######################MAIN######################################
+
+from Laberinto import *
+from Obstaculo import *
+from Robot import *
+#from Visualizacion import *
+import pygame, sys
+from pygame.locals import *
+#Importo asi para usar pygame.event (https://www.pygame.org/docs/ref/locals.html)
+
+
+dim_x = 50
+dim_y = 50
+
+laberinto = Laberinto(dim_x, dim_y)
+laberinto.generar_solucion()
+mapa = laberinto.mapa
+
+
+# Definimos algunos colores
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
+VERDE = ( 0, 255, 0)
+ROJO = (255, 0, 0)
+
+
+PIXEL_size = 50
+WIDTH = 1000
+HEIGHT = 1000
+
+#-------------INICIALIZACION DE GRAFICOS-----------------
+pygame.init()
+pantalla = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("Ada-BOT Maze")
+reloj = pygame.time.Clock()
+#--------------------------------------------------------
+
+no_ceros = np.nonzero(mapa)
+pantalla.fill(BLANCO)
+
+
+for j in range(len(no_ceros[0])):
+    pygame.draw.rect(pantalla, NEGRO,[no_ceros[0][j]*PIXEL_size, no_ceros[1][j]*PIXEL_size, PIXEL_size, PIXEL_size] )
+
+while True:
+    for eventos in pygame.event.get():
+        if eventos.type == QUIT:
+            sys.exit(0)
+
+    pygame.display.flip()
