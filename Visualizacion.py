e8600e46b10322d3280b3ba7438c6aca2b1d43f6
Santiago Maldonado Ochoa <santimaldonadoochoa@gmail.com>
2019-07-24 23:44:12 -0300

    Visualizacion. Logre achicar el tamano del icono

--- Visualizacion.py
+++ Visualizacion.py
@@ -2,36 +2,28 @@
 import pygame
 from pygame.locals import *
 from Laberinto import *
-"""
-probando hacer un commit
-"""
-
-class Visualizacion(object):
-  """
-  :version:
-  :author:
-  """
-  """ ATTRIBUTES
-  laberinto  (private)
-  """
-
-  def graficar(self):
-    """
-    @return  :
-    @author
-    """
-    pass
 
 class Icono (pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.imagen = pygame.image.load('Ada_bot.jpeg')
-        self.rect = self.imagen.get_rect()
+        self.rect = pygame.Rect((100,100), (20,20))
+        self.imagen = pygame.image.load('alien.jpeg')
+        #self.rect = self.imagen.get_rect()
         self.velocidad = 0.1
         self.direccion = (1,0)
-        self.rect.centerx = 100
-        self.rect.centery = 100
 
+
+    def set_tamano(self, tamano):
+        """
+        #Metodo
+         ------
+        set_tamano(tamano = *tuple* 2x1)
+
+          Define el tamano del icono del robot. La unidad de medida son *pixeles*.
+        El input es una tupla (ancho,alto)
+        """
+        imagen = self.imagen.convert_alpha()
+        self.imagen = pygame.transform.scale(imagen, tamano)
 
     def set_imagen(self,archivo):
         self.imagen = pygame.image.load(archivo)
@@ -56,7 +48,7 @@ class Icono (pygame.sprite.Sprite):
         self.rect.centery += self.direccion[1] * self.velocidad * tiempo
 ####################### funciones ######################################
 
-def cheaquear_si_cierro_ventana():
+def cheaquear_cierre_ventana():
     for eventos in pygame.event.get():
         if eventos.type == QUIT:
             sys.exit(0)
@@ -64,11 +56,9 @@ def cheaquear_si_cierro_ventana():
 #######################MAIN######################################
 
 from Laberinto import *
-from Obstaculo import *
 from Robot import *
 #from Visualizacion import *
-import pygame, sys
-from pygame.locals import *
+import sys
 #Importo asi para usar pygame.event (https://www.pygame.org/docs/ref/locals.html)
 
 dim_x = 30
@@ -82,43 +72,47 @@ PIXEL_size = 20
 WIDTH = PIXEL_size * dim_x
 HEIGHT = PIXEL_size * dim_y
 
-#-------------INICIALIZACION DE GRAFICOS-----------------
+#----------------------INICIALIZACION DE GRAFICOS-------------------------------
 pygame.init()
 pantalla = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Ada-BOT Maze")
 reloj = pygame.time.Clock()
-#--------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-######## esto en reliadad se hace con las clases, aca lo invento
-# invento un mapa random
-mapa= np.random.randint(0,2,(int(WIDTH/PIXEL_size), int(HEIGHT/PIXEL_size)))
-###############################################################
-
+#----------------------CREACION DEL LABERINTO-----------------------------------
+#############################################################################
+# invento un mapa random                                                    #
+#mapa= np.random.randint(0,2,(int(WIDTH/PIXEL_size), int(HEIGHT/PIXEL_size)))#
+mapa = np.zeros((int(WIDTH/PIXEL_size), int(HEIGHT/PIXEL_size)))
+mapa[int(100/PIXEL_size):int(300/PIXEL_size),int(100/PIXEL_size)] = 1
+#############################################################################
 no_ceros = np.nonzero(mapa)
 pantalla.fill(BLANCO)
-
 for j in range(len(no_ceros[0])):
     pygame.draw.rect(pantalla, NEGRO,[no_ceros[0][j]*PIXEL_size, no_ceros[1][j]*PIXEL_size, PIXEL_size, PIXEL_size] )
-
+# guardo el laberinto como una IMAGEN en la variable background
+# sirve para imprimir el laberinto
 background = pantalla.convert()
+#-------------------------------------------------------------------------------
 
-######cREO EL ICONO Robot
+#----------------------CREACION DEL ICONO ROBOT----------------------------------
 icono = Icono()
-icono.set_imagen("./Ada_bot.jpeg")
-pantalla.blit(icono.imagen, icono.rect)
+icono.set_imagen("./alien.jpeg")
+print(icono.rect, icono.get_posicion())
+icono.set_tamano((PIXEL_size, PIXEL_size))
+print(icono.rect, icono.get_posicion())
+pantalla.blit(icono.imagen,icono.rect)
 pygame.display.flip()
-pygame.time.wait(2000)
-#######
+# Espero un segundo antes de arrancar
+pygame.time.wait(1000)
+#-------------------------------------------------------------------------------
 
 
 nuevo = [(300,100), (300,200), (200,100 )]
 nueva_dir = [(1,0), (0,1), (-1,0)]
 i = 0
 while True:
-    #for eventos in pygame.event.get():
-    #    if eventos.type == QUIT:
-    #        sys.exit(0)
-    cheaquear_si_cierro_ventana()
+    cheaquear_cierre_ventana()
     #########################################
     # ACA VA EL BLOQUE QUE TOMA LA DECISION.#
     # ME DEVUELVE UNA DIRECCION             #
@@ -133,7 +127,7 @@ while True:
     condicion = True
     while condicion:
     #for i in range(10):
-        cheaquear_si_cierro_ventana()
+        cheaquear_cierre_ventana()
         tiempo = reloj.tick(60)
         posicion = icono.get_posicion()
         #print(posicion, new_xy)
